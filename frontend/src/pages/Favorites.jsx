601a18de26f0cac358d433b92eed0cc2a93ae5eb
noahbir <noah.birmanns@gmail.com>
2025-05-21 09:27:25 +0200

    css and page routing

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import "../css/Favorites.css"
+
+function Favorites() {
+    return (
+        <div className="favorites-empty">
+            <h2>No Favorite Movies Yet</h2>
+            <p>Add Movies to Favorites</p>
+        </div>
+    )
+}
+
+export default Favorites
